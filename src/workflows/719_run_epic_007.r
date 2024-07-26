71f5d337084aca2f18701a94bd8cb32ed60f8b37
federico-syngenta <federico.richard@syngenta.com>
2024-07-26 22:47:22 +0000

    auto commit 1       20240726 224721

--- src/workflows/719_run_epic_007.r
+++ src/workflows/719_run_epic_007.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/719_workflow_epic_f202109_006.r"
+PARAM <- "src/workflows/719_workflow_epic_f202109_007.r"
 
 envg <- env()
 
